127ad4e2f67b46aa20cb18dd40cfbd396d0e29c3
niluhputudewiratnasari <dewiiratnas12@gmail.com>
2021-03-24 19:35:32 +0700

    Validation Indonesia

--- application/views/templetes/sidebarindex.php
+++ application/views/templetes/sidebarindex.php
@@ -3,8 +3,9 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-hospital-alt"></i>
+        <div class="sidebar-brand-icon ">
+            <i class="fas fa-plus-square"></i>
+            <!-- <i class="fas fa-hospital-alt"></i> -->
         </div>
         <div class="sidebar-brand-text mx-3">Dinas Kesehatan</div>
     </a>
